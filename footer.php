2e16a4d98934f1e211d7c71afa4aa9f4cda22b02
bengodfrey43 <bengodfrey43@hotmail.com>
2025-03-15 20:45:34 +0800

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,17 @@
+        <footer class="container-fluid text-center">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6">
+                        <a class="navbar-brand" href="#"><span class="josh-white">JOSH</span> <span class="orange-whitkin">WHITKIN</span></a>
+                    </div>
+                    <div class="col-md-6">
+                        <p>Terms and Conditions</p>
+                    </div>
+                </div>
+            </div><!-- container -->
+        </footer><!-- container-fluid -->
+        
+        <?php wp_footer(); ?>
+
+    </body>
+</html>
